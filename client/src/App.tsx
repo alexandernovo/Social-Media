26c179f8e98cc10c6f1c74c63d2fb1a42a493083
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-03-02 22:48:39 +0800

    react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import MainLayout from './layout/MainLayout'
+import Login from './pages/Login'
+import NotFound from './pages/Login'
+import Home from './pages/Login'
+import PrivateRoutes from './middleware/PrivateRoutes'
 
 function App() {
   return (
     <MainLayout>
-      Hey
+      <Router>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route element={<Home />} path='/home' ></Route>
+          </Route>
+          <Route element={<Login />} path='/' ></Route>
+          <Route element={<NotFound />} path='*'></Route>
+        </Routes>
+      </Router>
     </MainLayout>
   )
 }
