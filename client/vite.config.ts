0b4d83a7d7392685b67171de2edc6b00bd67addc
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-03-08 21:20:30 +0800

    new alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
+        // target: 'https://chat-app-xsxm.onrender.com',
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
@@ -15,15 +16,16 @@ export default defineConfig({
       }
     }
   },
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname,'./src/'),
-      '@components' : path.resolve(__dirname,'./src/components'),
-      '@pages' : path.resolve(__dirname,'./src/pages'),
-      '@middleware' : path.resolve(__dirname,'./src/middleware'),
-      '@layout' : path.resolve(__dirname,'./src/layout'),
-      '@images' : path.resolve(__dirname,'./src/assets/images'),
-      '@assets' : path.resolve(__dirname,'./src/assets'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@middleware': path.resolve(__dirname, './src/middleware'),
+      '@layout': path.resolve(__dirname, './src/layout'),
+      '@images': path.resolve(__dirname, './src/assets/images'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@store': path.resolve(__dirname, './src/store'),
     }
   }
 })
