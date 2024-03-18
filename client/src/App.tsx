853a901e3000071df44f7ea41ba999308bc1808d
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-03-18 14:43:06 +0800

    Fix import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import MainLayout from '@layout/MainLayout'
 import Login from '@pages/Login'
 import NotFound from '@pages/NotFound'
-import Home from '@pages/Login'
+import Home from '@pages/Home'
 import PrivateRoutes from '@middleware/PrivateRoutes'
 
 function App() {
