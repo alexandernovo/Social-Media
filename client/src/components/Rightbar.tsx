7562ebdeb89a6309747cdfbbb9f7b9c285d1052b
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-03-19 23:22:10 +0800

    adjust width

--- client/src/components/Rightbar.tsx
+++ client/src/components/Rightbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sidebar: React.FC = () => {
     return (
-        <div className="w-[250px] h-[100%] position-sticky top-0 bg-[#F0F3F4] p-3">
+        <div className="w-[300px] h-[100%] position-sticky top-0 bg-[#F0F3F4] p-3">
             <ul>
                 <li></li>
                 <li></li>
