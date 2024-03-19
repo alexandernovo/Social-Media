0dc53d1e614e26e372de3610c34182e78d8a460b
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-03-19 23:22:25 +0800

    add class Props

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import { RootState } from '@store/store';
+interface SidebarProps {
+    Class?: string
+}
+const Sidebar: React.FC<SidebarProps> = (props: SidebarProps) => {
 
-const Sidebar: React.FC = () => {
+    const userState = useSelector((state: RootState) => state.userState);
+    const name = `${userState.firstname} ${userState.lastname}`;
+    const classes = `${props.Class} w-[300px] h-[100%] bg-[#F0F3F4] p-3 pt-4`;
+
     return (
-        <div className="w-[250px] h-[100%] position-sticky top-0 bg-[#F0F3F4] p-3 pt-4">
+        <div className={classes}>
             <ul>
                 <li>
                     <div className="flex items-center gap-2">
                         <div className='w-[35px] h-[35px] overflow-hidden border border-gray-300 rounded-full'>
                         </div>
-                        <p className="text-[14px] font-semibold">Alexander Novo</p>
+                        <p className="text-[14px] font-semibold">{name}</p>
                     </div>
                 </li>
                 <li></li>
